416a99f6189b8b0eff4e374568d9a6a958593031
Brandt Reutimann <brandtr@austin.cs.colostate.edu>
2018-04-22 15:11:19 -0600

    Current state

--- src/main/scala/yelp/YelpAnalyzer.scala
+++ src/main/scala/yelp/YelpAnalyzer.scala
@@ -1,19 +1,28 @@
 package yelp
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
 /**
  * To run:
  * spark-submit --class yelp.YelpAnalyzer ./target/original-yelp-reviews-1.0.jar \ 
  * <input-file-from-hdfs> \
  * <output-file-from-hdfs>
  */
+
+/**
+ * Review schema
+ */
+
 object YelpAnalyzer {
     def main (args: Array[String]): Unit = {
-        // Create new spark context
-        val sc: SparkContext = new SparkContext(new SparkConf().setAppName("Spark Count"))
-        // Create an RDD from the input text file 
-        val inputRDD = sc.textFile("")
+        // Start a spark session
+		val spark = SparkSession.builder().appName("Analyze Yelp Data").getOrCreate()
+		// Reviews
+		val reviews = spark.read.
+              option("header", "true").
+              option("delimiter", ",").
+              csv("/yelp-data/yelp_review.csv")
+    reviews.groupBy
+              
     }
 }
